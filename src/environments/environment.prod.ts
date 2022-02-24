4900e18f7f528af9932d122fd6cb3933cac7cc11
HaolongHuai <1612907625@qq.com>
2022-02-25 01:18:51 +0800

    change13

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
-/*
- * @Author: your name
- * @Date: 2022-02-24 18:51:35
- * @LastEditTime: 2022-02-25 00:48:54
- * @LastEditors: your name
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \docker-deploy-frontend\src\environments\environment.prod.ts
- */
 export const environment = {
   production: true,
-  graphqlUri: 'http://52.74.162.217:8085/graphql'
+  graphqlUri: 'http://18.212.59.252:8085/graphql'
 
 };
 
